c81972f32e3c69475c02fe1d7ba554fe8859641d
Roman Tazhdynov <105038717+tajdinov@users.noreply.github.com>
2022-10-21 00:52:52 +1030

    updated package.json and app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import AboutMe from "./components/AboutMe";
+import Experience from './components/Experience';
+import Portfolio from './components/Portfolio';
+import Contacts from "./components/Contacts";
+import Footer from "./components/Footer"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+    <Navbar /> 
+    <Header />
+    <AboutMe />
+    <Experience/> 
+    <Portfolio /> 
+    <Contacts/>
+    <Footer /> 
+  </>
   );
 }
 
